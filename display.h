349e08f3e5d7b31053dbdbf25258f14a802ec1f7
jahaziel2903 <55322568+jahaziel2903@users.noreply.github.com>
2020-03-21 12:45:44 -0600

    Cambios

--- display.h
+++ display.h
@@ -19,6 +19,17 @@
 #include "Push_Buttons.h"
 #include "PWMrgb.h"
 
+//Se divide en 5 partes, rojo-amarillo, amarillo-verde
+//verde-azul, azul-morada, morado 255/5 = 51
+#define RED_YELLOW (0x33u) //51
+#define YELLOW_GREEN (0x66u) //102
+#define GREEN_BLUE (0x99u) //153
+#define BLUE_PURPLE (0xCCu) //204
+#define PURPLE (0xFFu) //255
+
+//como son valores de 0 a 3.3, se hace un cast 0 - 255
+#define cast_255 (0x5u)
+
 /** Constant that represent different program states*/
 typedef enum {
 		imagen,
