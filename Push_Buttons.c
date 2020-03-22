fbadb3339cb56763e2458c29d7f361da6fb9aaff
jahaziel2903 <55322568+jahaziel2903@users.noreply.github.com>
2020-03-22 15:40:42 -0600

    cambios

--- Push_Buttons.c
+++ Push_Buttons.c
@@ -1,9 +1,18 @@
 
-#include "Push_Buttons.h"
 
+#include "Push_Buttons.h"
+#include "Delay.h"
+#include "stdint.h"
+#include "GPIO.h"
+#include "MK64F12.h"
+#include "Bits.h"
+#include "Push_Buttons.h"
+#include "LCD_nokia.h"
+#include "display.h"
+#include "flextimer.h"
 static Buttons_t current_button = NO_PB;
 
-
+button_flag_t g_button_flag = {FALSE};
 
 void Push_Buttons_init()
 {
@@ -12,13 +21,14 @@ void Push_Buttons_init()
 	GPIO_clock_gating(GPIO_C);
 	GPIO_clock_gating(GPIO_D);
 
-	gpio_pin_control_register_t PCR_GPIO_in_ports = GPIO_MUX1|INTR_RISING_EDGE|GPIO_PE;
+	gpio_pin_control_register_t PCR_GPIO_in_ports = GPIO_MUX1|INTR_FALLING_EDGE|GPIO_PE | GPIO_PS;
 
 	GPIO_pin_control_register(GPIO_C, bit_6, &PCR_GPIO_in_ports);//SW2
 	GPIO_data_direction_pin(GPIO_C, GPIO_INPUT, bit_6);
 
 	GPIO_pin_control_register(GPIO_A, bit_4, &PCR_GPIO_in_ports);//SW4
 	GPIO_data_direction_pin(GPIO_A, GPIO_INPUT, bit_4);
+
 
 
 	GPIO_pin_control_register(GPIO_C, bit_5, &PCR_GPIO_in_ports);
@@ -33,11 +43,11 @@ void Push_Buttons_init()
 	GPIO_pin_control_register(GPIO_D, bit_3, &PCR_GPIO_in_ports);
 	GPIO_data_direction_pin(GPIO_D, GPIO_INPUT, bit_3);
 
-	GPIO_pin_control_register(GPIO_E, bit_25, &PCR_GPIO_in_ports);
-	GPIO_data_direction_pin(GPIO_E, GPIO_INPUT, bit_25);
+	GPIO_pin_control_register(GPIO_B, bit_11, &PCR_GPIO_in_ports);
+	GPIO_data_direction_pin(GPIO_B, GPIO_INPUT, bit_11);
 
-	GPIO_pin_control_register(GPIO_E, bit_24, &PCR_GPIO_in_ports);
-	GPIO_data_direction_pin(GPIO_E, GPIO_INPUT, bit_24);
+	GPIO_pin_control_register(GPIO_C, bit_11, &PCR_GPIO_in_ports);
+	GPIO_data_direction_pin(GPIO_C, GPIO_INPUT, bit_11);
 
 	GPIO_pin_control_register(GPIO_C, bit_10, &PCR_GPIO_in_ports);
 	GPIO_data_direction_pin(GPIO_C, GPIO_INPUT, bit_10);
@@ -48,63 +58,116 @@ void Push_Buttons_init()
 	GPIO_pin_control_register(GPIO_B, bit_20, &PCR_GPIO_in_ports);
 	GPIO_data_direction_pin(GPIO_B, GPIO_INPUT, bit_20);
 
+	/*In case there is an interruption we pass to know which button was pressed**/
+	GPIO_callback_init(GPIO_A, Push_Buttons_readA);
+	GPIO_callback_init(GPIO_B, Push_Buttons_readB);
+	GPIO_callback_init(GPIO_C, Push_Buttons_readC);
+	GPIO_callback_init(GPIO_D, Push_Buttons_readD);
+
+	/*We configure interruptions**/
+	NVIC_enable_interrupt_and_priotity(PORTA_IRQ, PRIORITY_1);
+	NVIC_enable_interrupt_and_priotity(PORTB_IRQ, PRIORITY_1);
+	NVIC_enable_interrupt_and_priotity(PORTC_IRQ, PRIORITY_1);
+	NVIC_enable_interrupt_and_priotity(PORTD_IRQ, PRIORITY_1);
+	NVIC_global_enable_interrupts;
+
+
 }
 
 
 
-Buttons_t Push_Buttons_read()
+void Push_Buttons_readA()
 {
 	uint32_t input_value;
+	delay(100);
+	current_button = SW3;
+	delay(100);
+	g_button_flag.read_button_flag = TRUE;
+}
 
-		input_value = GPIO_read_port(GPIO_A);
-		input_value = READ_BUTTONSA & input_value;
+void Push_Buttons_readB()
+{
+	delay(100);
+	current_button = PB_4;
+	delay(100);
+	g_button_flag.read_button_flag = TRUE;
+}
 
-		if(switch3 == input_value)
-		{
-			current_button = SW3;
-		}
+void Push_Buttons_readC()
+{
+	uint32_t input_value;
+	delay(100);
+	input_value = GPIO_read_port(GPIO_C);
+	input_value = READ_BUTTONSC & input_value;
 
-		input_value = GPIO_read_port(GPIO_B);
-		input_value = READ_BUTTONSB & input_value;
+	if(BUTTON_0 == input_value)
+	{
+		current_button = PB_0;
+		printf("pb1");
+	}
+	if(BUTTON_1 == input_value)
+	{
+		current_button = PB_1;
+		printf("pb1");
+	}
+	if(switch2 == input_value)
+	{
+		current_button = SW2;
+		printf("sw2");
+	}
+	if(BUTTON_6 == input_value)
+	{
+		current_button = PB_6;
+		printf("pb6");
+	}
+	if(BUTTON_5 == input_value)
+	{
+		current_button = PB_5;
+		printf("pb5");
+	}
+	delay(100);
+	g_button_flag.read_button_flag = TRUE;
+}
 
-		if(BUTTON_4 == input_value)
-		{
-			current_button = PB_4;
-		}
+void Push_Buttons_readD()
+{
+	uint32_t input_value;
+	delay(100);
 
-		input_value = GPIO_read_port(GPIO_C);
-		input_value = READ_BUTTONSC & input_value;
+	input_value = GPIO_read_port(GPIO_D);
+	input_value = READ_BUTTONSD & input_value;
 
-		if(BUTTON_1 == input_value)
-		{
-			current_button = PB_1;
-		}
-		if(switch2 == input_value)
-		{
-			current_button = SW2;
-		}
-		if(BUTTON_6 == input_value)
-		{
-			current_button = PB_6;
-		}
-		if(BUTTON_5 == input_value)
-		{
-			current_button = PB_5;
-		}
+	if(BUTTON_2 == input_value)
+	{
+		current_button = PB_2;
+		printf("pb2");
+	}
 
-		input_value = GPIO_read_port(GPIO_D);
-		input_value = READ_BUTTONSD & input_value;
-
-		if(BUTTON_2 == input_value)
-		{
-			current_button = PB_2;
-		}
-
-		if(BUTTON_3 == input_value)
-		{
-			current_button = PB_3;
-		}
-
-		return current_button;
+	if(BUTTON_3 == input_value)
+	{
+		current_button = PB_3;
+			printf("pb3");
+	}
+	delay(100);
+	g_button_flag.read_button_flag = TRUE;
 
 }
+
+uint8_t Buttons_get_read_button_flag(void)
+{
+	/**Variable to capture the flag value*/
+	uint8_t flag_value = g_button_flag.read_button_flag;
+	/**Rest the flag value*/
+	g_button_flag.read_button_flag = FALSE;
+	return flag_value;
+}
+
+Buttons_t get_key()
+{
+	Buttons_t temp = current_button ;
+	printf("%d", temp);
+	current_button = NO_PB;
+	return temp;
+
+}
+
