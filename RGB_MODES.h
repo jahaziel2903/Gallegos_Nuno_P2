4686c03a9aedc01cce92c461914daee002e83261
jahaziel2903 <55322568+jahaziel2903@users.noreply.github.com>
2020-03-31 13:17:53 -0600

    FINAL

--- RGB_MODES.h
+++ RGB_MODES.h
@@ -8,7 +8,13 @@
 #ifndef RGB_MODES_H_
 #define RGB_MODES_H_
 
-
+//Se divide en 5 partes, rojo-amarillo, amarillo-verde
+//verde-azul, azul-morada, morado 255/5 = 51
+#define RED_YELLOW (0x33u) //51
+#define YELLOW_GREEN (0x66u) //102
+#define GREEN_BLUE (0x99u) //153
+#define BLUE_PURPLE (0xCCu) //204
+#define PURPLE (0xFFu) //255
 #define cast 25.5
 
 
